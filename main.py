cc90cecf9ab80e7e12dbaba943a3933b5d482760
CozyCloud22 <DCbankai888@gmail.com>
2023-05-07 17:52:05 -0400

    final commit with comments

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ api_key = config.API_KEY
 city = "East Lansing"
 url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
 
+#recieving responses from the api
 response = requests.get(url)
 data = response.json()
 
@@ -17,17 +18,21 @@ timeDiff = timeZoneOff / 3600
 
 sunrise = data["sys"]["sunset"]
 
+#converts the sunset time to a time that 
 convertReg = datetime.datetime.utcfromtimestamp(sunrise)
 timern = convertReg.strftime("%H:%M:%S")
 toChange = timern[0:2]
 
+#calculates time zone difference
 if(toChange == "00"):
     toChange = "12"
 temp = int(toChange)
 temp += int(timeDiff)
+
 if(temp < 1):
     temp += 12
 subBackIn = str(temp)
 finalAns = subBackIn + timern[2:-3] + " pm"
 
+#prints answer
 print(finalAns)
